489f189b63d79f43b961cdcd05b0fca74da2bdd6
annie-jennemann <Annie.Jennemann@hearst.com>
2026-02-26 10:50:56 -0500

    Add files via upload

---
+++ power.R
@@ -0,0 +1,19 @@
+library(httr2)
+library(readr)
+library(tidyverse)
+
+api_key <- Sys.getenv("API_KEY")
+us_map <- Sys.getenv("CHART_CODE")
+
+datawrapper_auth(api_key =  api_key, overwrite=TRUE)
+
+url <- "http://mema.mapsonline.net/power_outage_public.csv"
+
+resp <- request(url) |>
+  req_user_agent("Mozilla/5.0 (GitHub Actions; R)") |>
+  req_retry(max_tries = 8) |>
+  req_timeout(120) |>
+  req_perform()
+
+dat <- read_csv(resp_body_raw(resp), show_col_types = FALSE)
+print(head(dat))
