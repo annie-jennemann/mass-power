ababe9690dcb4dbcd115f441f4a1a317ce928374
annie-jennemann <Annie.Jennemann@hearst.com>
2026-02-27 11:03:48 -0500

    Format timestamp and update chart annotation

--- power.R
+++ power.R
@@ -19,10 +19,21 @@ total <- format(total, big.mark = ",", scientific = FALSE)
 
 api_key <- Sys.getenv("API_KEY")
 
+et_time <- as.POSIXlt(Sys.time(), tz = "America/New_York")
+
+formatted_time <- format(et_time, "%B %d at %I:%M %p EST")
+
+formatted_time <- sub(" 0", " ", formatted_time)   # day
+formatted_time <- sub(" at 0", " at ", formatted_time) # hour
+
+formatted_time <- gsub("AM", "a.m.", formatted_time)
+formatted_time <- gsub("PM", "p.m.", formatted_time)
+
 datawrapper_auth(api_key =  api_key, overwrite=TRUE)
 
 dw_edit_chart("cP5ai",
-              title = paste0("<b>",total,"</b> customers are without power in Massachusetts")
+              title = paste0("<b>",total,"</b> customers are without power in Massachusetts"),
+              annotate = paste("Chart updated ", formatted_time)
 )
 
 dw_publish_chart("cP5ai")
